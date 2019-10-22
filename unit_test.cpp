9229bf139ec469bfcd63e280b9bc89c315e21b51
Linus Im <lim001@ucr.edu>
2019-10-22 16:37:03 -0700

    Slightly altered unit_test file to include all unit tests

--- unit_test.cpp
+++ unit_test.cpp
@@ -3,9 +3,9 @@
 #include "op_test.hpp"
 #include "mult_test.hpp"
 #include "div_test.hpp"
-//#include "add_test.hpp"
-//#include "sub_test.hpp"
-//#include "pow_test.hpp"
+#include "add_test.hpp"
+#include "sub_test.hpp"
+#include "pow_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
