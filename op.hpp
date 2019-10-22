52d51eb076cbbdd9dac39d301a731b7acd0a8d37
Linus Im <lim001@ucr.edu>
2019-10-22 15:26:26 -0700

    Finished adding new files

--- op.hpp
+++ op.hpp
@@ -1,45 +1,61 @@
 #ifndef __OP_HPP__
 #define __OP_HPP__
-
+#include <string>
 #include "base.hpp"
 
 class Op : public Base {
+    private:
+	double val;
+
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value);
+        virtual double evaluate() { return value; }
+        virtual std::string stringify() { return ->stringify() }
 };
+
+Op::Op(double value)
+{
+	val = value;
+}
 
 class SevenOpMock: public Base {
     public:
         SevenOpMock() { };
 
         virtual double evaluate() { return 7.5; }
-        virtual string stringify() { return "7.5"; }
-}
+        virtual std::string stringify() { return "7.5"; }
+};
 
 class NegativeOpMock: public Base {
     public:
         NegativeOpMock() { };
 
         virtual double evaluate() { return -7.5; }
-        virtual string stringify() { return "-7.5"; }
-}
+        virtual std::string stringify() { return "-7.5"; }
+};
 
 class ZeroOpMock: public Base {
     public:
         ZeroOpMock() { };
 
         virtual double evaluate() { return 0; }
-        virtual string stringify() { return "0"; }
-}
+        virtual std::string stringify() { return "0"; }
+};
 
 class TwentySixOpMock: public Base {
     public:
         TwentySixOpMock() { };
 
         virtual double evaluate() { return 26; }
-        virtual string stringify() { return "26"; }
-}
+        virtual std::string stringify() { return "26"; }
+};
+
+class EightMock: public Base {
+    public:
+	EightMock() { };
+	
+	virtual double evaluate() { return 8; }
+	virtual std::string stringify() { return "8"; }
+};
 
 #endif //__OP_HPP__
